637b6e6244857686f36fefd674e841659d45d93d
Howard Selfe <hselfe@ideationai.com>
2020-02-05 16:30:12 +0200

    Sticky strat 1, contact form 90% complete

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from forms import ContactForm
 from bson import json_util
 import json
 
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
@@ -17,7 +19,16 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config["MONGO_URI"] = "mongodb+srv://hselfe:pymongo2112@cluster0-ru7km.mongodb.net/test?retryWrites=true&w=majority/"
 pymongo = PyMongo(app)
 
+# Mail
 app.secret_key = 'development_key'
+mail = Mail()
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'hselfe@gmail.com'
+app.config["MAIL_PASSWORD"] = 'Accra@2018a'
+mail.init_app(app)
+# Mail
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
@@ -27,7 +38,13 @@ def contact():
             flash('All fields are required.')
             return render_template('contact.html', form=form)
         else:
-            return 'Form posted.'
+            msg = Message(form.subject.data, sender='hselfe@gmail.com', recipients=['selfe.j@pg.com'])
+            msg.body = """
+      From: %s &lt;%s&gt;
+      %s
+      """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+            return render_template('contact.html', success=True)
     elif request.method == 'GET':
         return render_template('contact.html', form=form)
 
